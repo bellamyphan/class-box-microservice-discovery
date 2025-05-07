87b5d9ca8c9f1a848d293cc77a04010ceb917013
BellamyPhan <bellamyphan@icloud.com>
2025-05-06 19:58:58 -0500

    Initial commit

---
+++ src/main/java/com/bellamyphan/microservice_discovery/MicroserviceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.bellamyphan.microservice_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bellamyphan/microservice_discovery/MicroserviceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellamyphan.microservice_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
