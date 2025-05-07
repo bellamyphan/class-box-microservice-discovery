622cf4a659ab0e19ffe75c5ecf9f9c35ddaeab8c
BellamyPhan <bellamyphan@icloud.com>
2025-05-06 20:26:55 -0500

    Register this project as Eureka Server

--- src/main/java/com/bellamyphan/microservice_discovery/MicroserviceDiscoveryApplication.java
+++ src/main/java/com/bellamyphan/microservice_discovery/MicroserviceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.bellamyphan.microservice_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroserviceDiscoveryApplication {
 
 	public static void main(String[] args) {
